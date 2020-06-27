a906f3debbf4f13dd654a5a71085650531f606b8
decaffeinate <trustmaster@kodigy.com>
2020-06-27 18:57:45 +0200

    decaffeinate: Convert Platform.coffee to JS

--- src/lib/Platform.js
+++ src/lib/Platform.js
@@ -1,21 +1,31 @@
-#     NoFlo - Flow-Based Programming for JavaScript
-#     (c) 2014-2017 Flowhub UG
-#     NoFlo may be freely distributed under the MIT license
-#
-# Platform detection method
-exports.isBrowser = ->
-  if typeof process isnt 'undefined' and process.execPath and process.execPath.match /node|iojs/
-    return false
-  true
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+//     NoFlo - Flow-Based Programming for JavaScript
+//     (c) 2014-2017 Flowhub UG
+//     NoFlo may be freely distributed under the MIT license
+//
+// Platform detection method
+exports.isBrowser = function() {
+  if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
+    return false;
+  }
+  return true;
+};
 
-# Mechanism for showing API deprecation warnings. By default logs the warnings
-# but can also be configured to throw instead with the `NOFLO_FATAL_DEPRECATED`
-# env var.
-exports.deprecated = (message) ->
-  if exports.isBrowser()
-    throw new Error message if window.NOFLO_FATAL_DEPRECATED
-    console.warn message
-    return
-  if process.env.NOFLO_FATAL_DEPRECATED
-    throw new Error message
-  console.warn message
+// Mechanism for showing API deprecation warnings. By default logs the warnings
+// but can also be configured to throw instead with the `NOFLO_FATAL_DEPRECATED`
+// env var.
+exports.deprecated = function(message) {
+  if (exports.isBrowser()) {
+    if (window.NOFLO_FATAL_DEPRECATED) { throw new Error(message); }
+    console.warn(message);
+    return;
+  }
+  if (process.env.NOFLO_FATAL_DEPRECATED) {
+    throw new Error(message);
+  }
+  return console.warn(message);
+};
