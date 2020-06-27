80d86a7711436a2f9aad31ae2c1ab45a32bd3717
decaffeinate <trustmaster@kodigy.com>
2020-06-27 18:57:47 +0200

    decaffeinate: Run post-processing cleanups on Platform.coffee

--- src/lib/Platform.js
+++ src/lib/Platform.js
@@ -1,3 +1,11 @@
+/* eslint-disable
+    consistent-return,
+    func-names,
+    no-console,
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
@@ -8,7 +16,7 @@
 //     NoFlo may be freely distributed under the MIT license
 //
 // Platform detection method
-exports.isBrowser = function() {
+exports.isBrowser = function () {
   if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
     return false;
   }
@@ -18,7 +26,7 @@ exports.isBrowser = function() {
 // Mechanism for showing API deprecation warnings. By default logs the warnings
 // but can also be configured to throw instead with the `NOFLO_FATAL_DEPRECATED`
 // env var.
-exports.deprecated = function(message) {
+exports.deprecated = function (message) {
   if (exports.isBrowser()) {
     if (window.NOFLO_FATAL_DEPRECATED) { throw new Error(message); }
     console.warn(message);
