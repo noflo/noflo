e93da91b76208c3bb6dc126a63c0c7ab1a2b6705
decaffeinate <trustmaster@kodigy.com>
2020-06-27 16:26:03 +0200

    decaffeinate: Convert register.coffee to JS

--- src/lib/loader/register.js
+++ src/lib/loader/register.js
@@ -1,5 +1,6 @@
-{isBrowser} = require '../Platform'
-if isBrowser()
-  throw new Error 'Generate NoFlo component loader for browsers with noflo-component-loader'
-else
-  module.exports = require './NodeJs'
+const {isBrowser} = require('../Platform');
+if (isBrowser()) {
+  throw new Error('Generate NoFlo component loader for browsers with noflo-component-loader');
+} else {
+  module.exports = require('./NodeJs');
+}
