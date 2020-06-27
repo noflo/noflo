c4e2215b67b46e45d1ab0e3751aeb373f71a022a
decaffeinate <trustmaster@kodigy.com>
2020-06-27 16:26:05 +0200

    decaffeinate: Run post-processing cleanups on register.coffee

--- src/lib/loader/register.js
+++ src/lib/loader/register.js
@@ -1,4 +1,11 @@
-const {isBrowser} = require('../Platform');
+/* eslint-disable
+    global-require,
+    import/no-unresolved,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
+const { isBrowser } = require('../Platform');
+
 if (isBrowser()) {
   throw new Error('Generate NoFlo component loader for browsers with noflo-component-loader');
 } else {
