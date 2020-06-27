55bcde1446f48c763b75b2b8cae882b139f3829c
decaffeinate <trustmaster@kodigy.com>
2020-06-27 16:38:37 +0200

    decaffeinate: Convert NodeJs.coffee to JS

--- src/lib/loader/NodeJs.js
+++ src/lib/loader/NodeJs.js
@@ -1,247 +1,310 @@
-path = require 'path'
-fs = require 'fs'
-manifest = require 'fbp-manifest'
-utils = require '../Utils'
-fbpGraph = require 'fbp-graph'
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const path = require('path');
+const fs = require('fs');
+const manifest = require('fbp-manifest');
+const utils = require('../Utils');
+const fbpGraph = require('fbp-graph');
 
-# We allow components to be un-compiled CoffeeScript
-CoffeeScript = require 'coffeescript'
-if typeof CoffeeScript.register != 'undefined'
-  CoffeeScript.register()
+// We allow components to be un-compiled CoffeeScript
+const CoffeeScript = require('coffeescript');
+if (typeof CoffeeScript.register !== 'undefined') {
+  CoffeeScript.register();
+}
 
-registerCustomLoaders = (loader, componentLoaders, callback) ->
-  unless componentLoaders.length
-    callback null
-    return
-  customLoader = require componentLoaders.shift()
-  loader.registerLoader customLoader, (err) ->
-    if err
-      callback err
-      return
-    registerCustomLoaders loader, componentLoaders, callback
-    return
-  return
+var registerCustomLoaders = function(loader, componentLoaders, callback) {
+  if (!componentLoaders.length) {
+    callback(null);
+    return;
+  }
+  const customLoader = require(componentLoaders.shift());
+  loader.registerLoader(customLoader, function(err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    registerCustomLoaders(loader, componentLoaders, callback);
+  });
+};
 
-registerModules = (loader, modules, callback) ->
-  compatible = modules.filter (m) -> m.runtime in ['noflo', 'noflo-nodejs']
-  componentLoaders = []
-  for m in compatible
-    loader.setLibraryIcon m.name, m.icon if m.icon
+const registerModules = function(loader, modules, callback) {
+  const compatible = modules.filter(m => ['noflo', 'noflo-nodejs'].includes(m.runtime));
+  const componentLoaders = [];
+  for (let m of Array.from(compatible)) {
+    if (m.icon) { loader.setLibraryIcon(m.name, m.icon); }
 
-    if m.noflo?.loader
-      loaderPath = path.resolve loader.baseDir, m.base, m.noflo.loader
-      componentLoaders.push loaderPath
+    if (m.noflo != null ? m.noflo.loader : undefined) {
+      const loaderPath = path.resolve(loader.baseDir, m.base, m.noflo.loader);
+      componentLoaders.push(loaderPath);
+    }
 
-    for c in m.components
-      loader.registerComponent m.name, c.name, path.resolve loader.baseDir, c.path
+    for (let c of Array.from(m.components)) {
+      loader.registerComponent(m.name, c.name, path.resolve(loader.baseDir, c.path));
+    }
+  }
 
-  registerCustomLoaders loader, componentLoaders, callback
-  return
+  registerCustomLoaders(loader, componentLoaders, callback);
+};
 
-manifestLoader =
-  writeCache: (loader, options, manifest, callback) ->
-    filePath = path.resolve loader.baseDir, options.manifest
-    fs.writeFile filePath, JSON.stringify(manifest, null, 2),
-      encoding: 'utf-8'
-    , callback
-    return
+const manifestLoader = {
+  writeCache(loader, options, manifest, callback) {
+    const filePath = path.resolve(loader.baseDir, options.manifest);
+    fs.writeFile(filePath, JSON.stringify(manifest, null, 2),
+      {encoding: 'utf-8'}
+    , callback);
+  },
 
-  readCache: (loader, options, callback) ->
-    options.discover = false
-    manifest.load.load loader.baseDir, options, callback
-    return
+  readCache(loader, options, callback) {
+    options.discover = false;
+    manifest.load.load(loader.baseDir, options, callback);
+  },
 
-  prepareManifestOptions: (loader) ->
-    loader.options = {} unless loader.options
-    options = {}
-    options.runtimes = loader.options.runtimes or []
-    options.runtimes.push 'noflo' if options.runtimes.indexOf('noflo') is -1
-    options.recursive = if typeof loader.options.recursive is 'undefined' then true else loader.options.recursive
-    options.manifest = loader.options.manifest or 'fbp.json'
-    return options
+  prepareManifestOptions(loader) {
+    if (!loader.options) { loader.options = {}; }
+    const options = {};
+    options.runtimes = loader.options.runtimes || [];
+    if (options.runtimes.indexOf('noflo') === -1) { options.runtimes.push('noflo'); }
+    options.recursive = typeof loader.options.recursive === 'undefined' ? true : loader.options.recursive;
+    options.manifest = loader.options.manifest || 'fbp.json';
+    return options;
+  },
 
-  listComponents: (loader, manifestOptions, callback) ->
-    @readCache loader, manifestOptions, (err, manifest) =>
-      if err
-        unless loader.options.discover
-          callback err 
-          return
-        dynamicLoader.listComponents loader, manifestOptions, (err, modules) =>
-          if err
-            callback err
-            return
-          @writeCache loader, manifestOptions,
-            version: 1
-            modules: modules
-          , (err) ->
-            if err
-              callback err
-              return
-            callback null, modules
-            return
-        return
-      registerModules loader, manifest.modules, (err) ->
-        if err
-          callback err
-          return
-        callback null, manifest.modules
-        return
-      return
-    return
+  listComponents(loader, manifestOptions, callback) {
+    this.readCache(loader, manifestOptions, (err, manifest) => {
+      if (err) {
+        if (!loader.options.discover) {
+          callback(err); 
+          return;
+        }
+        dynamicLoader.listComponents(loader, manifestOptions, (err, modules) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+          return this.writeCache(loader, manifestOptions, {
+            version: 1,
+            modules
+          }
+          , function(err) {
+            if (err) {
+              callback(err);
+              return;
+            }
+            callback(null, modules);
+          });
+        });
+        return;
+      }
+      registerModules(loader, manifest.modules, function(err) {
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null, manifest.modules);
+      });
+    });
+  }
+};
 
-dynamicLoader =
-  listComponents: (loader, manifestOptions, callback) ->
-    manifestOptions.discover = true
-    manifest.list.list loader.baseDir, manifestOptions, (err, modules) =>
-      if err
-        callback err
-        return
-      registerModules loader, modules, (err) ->
-        if err
-          callback err
-          return
-        callback null, modules
-        return
-      return
-    return
+var dynamicLoader = {
+  listComponents(loader, manifestOptions, callback) {
+    manifestOptions.discover = true;
+    manifest.list.list(loader.baseDir, manifestOptions, (err, modules) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      registerModules(loader, modules, function(err) {
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null, modules);
+      });
+    });
+  }
+};
 
-registerSubgraph = (loader) ->
-  # Inject subgraph component
-  if path.extname(__filename) is '.js'
-    graphPath = path.resolve __dirname, '../../components/Graph.js'
-  else
-    graphPath = path.resolve __dirname, '../../components/Graph.coffee'
-  loader.registerComponent null, 'Graph', graphPath
-  return
+const registerSubgraph = function(loader) {
+  // Inject subgraph component
+  let graphPath;
+  if (path.extname(__filename) === '.js') {
+    graphPath = path.resolve(__dirname, '../../components/Graph.js');
+  } else {
+    graphPath = path.resolve(__dirname, '../../components/Graph.coffee');
+  }
+  loader.registerComponent(null, 'Graph', graphPath);
+};
 
-exports.register = (loader, callback) ->
-  manifestOptions = manifestLoader.prepareManifestOptions loader
+exports.register = function(loader, callback) {
+  const manifestOptions = manifestLoader.prepareManifestOptions(loader);
 
-  if loader.options?.cache
-    manifestLoader.listComponents loader, manifestOptions, (err, modules) ->
-      if err
-        callback err
-        return
-      registerSubgraph loader
-      callback null, modules
-      return
-    return
+  if (loader.options != null ? loader.options.cache : undefined) {
+    manifestLoader.listComponents(loader, manifestOptions, function(err, modules) {
+      if (err) {
+        callback(err);
+        return;
+      }
+      registerSubgraph(loader);
+      callback(null, modules);
+    });
+    return;
+  }
 
-  dynamicLoader.listComponents loader, manifestOptions, (err, modules) ->
-    if err
-      callback err
-      return
-    registerSubgraph loader
-    callback null, modules
-    return
-  return
+  dynamicLoader.listComponents(loader, manifestOptions, function(err, modules) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    registerSubgraph(loader);
+    callback(null, modules);
+  });
+};
 
-exports.dynamicLoad = (name, cPath, metadata, callback) ->
-  try
-    implementation = require cPath
-  catch e
-    callback e
-    return
+exports.dynamicLoad = function(name, cPath, metadata, callback) {
+  let e, implementation, instance;
+  try {
+    implementation = require(cPath);
+  } catch (error) {
+    e = error;
+    callback(e);
+    return;
+  }
 
-  if typeof implementation.getComponent is 'function'
-    try
-      instance = implementation.getComponent metadata
-    catch e
-      callback e
-      return
-  else if typeof implementation is 'function'
-    try
-      instance = implementation metadata
-    catch e
-      callback e
-      return
-  else
-    callback new Error "Unable to instantiate #{cPath}"
-    return
-  instance.componentName = name if typeof name is 'string'
-  callback null, instance
-  return
+  if (typeof implementation.getComponent === 'function') {
+    try {
+      instance = implementation.getComponent(metadata);
+    } catch (error1) {
+      e = error1;
+      callback(e);
+      return;
+    }
+  } else if (typeof implementation === 'function') {
+    try {
+      instance = implementation(metadata);
+    } catch (error2) {
+      e = error2;
+      callback(e);
+      return;
+    }
+  } else {
+    callback(new Error(`Unable to instantiate ${cPath}`));
+    return;
+  }
+  if (typeof name === 'string') { instance.componentName = name; }
+  callback(null, instance);
+};
 
-exports.setSource = (loader, packageId, name, source, language, callback) ->
-  Module = require 'module'
-  if language is 'coffeescript'
-    try
-      source = CoffeeScript.compile source,
-        bare: true
-    catch e
-      callback e
-      return
-  try
-    # Use the Node.js module API to evaluate in the correct directory context
-    modulePath = path.resolve loader.baseDir, "./components/#{name}.js"
-    moduleImpl = new Module modulePath, module
-    moduleImpl.paths = Module._nodeModulePaths path.dirname modulePath
-    moduleImpl.filename = modulePath
-    moduleImpl._compile source, modulePath
-    implementation = moduleImpl.exports
-  catch e
-    callback e
-    return
-  unless typeof implementation is 'function' or typeof implementation.getComponent is 'function'
-    callback new Error 'Provided source failed to create a runnable component'
-    return
+exports.setSource = function(loader, packageId, name, source, language, callback) {
+  let e, implementation;
+  const Module = require('module');
+  if (language === 'coffeescript') {
+    try {
+      source = CoffeeScript.compile(source,
+        {bare: true});
+    } catch (error) {
+      e = error;
+      callback(e);
+      return;
+    }
+  }
+  try {
+    // Use the Node.js module API to evaluate in the correct directory context
+    const modulePath = path.resolve(loader.baseDir, `./components/${name}.js`);
+    const moduleImpl = new Module(modulePath, module);
+    moduleImpl.paths = Module._nodeModulePaths(path.dirname(modulePath));
+    moduleImpl.filename = modulePath;
+    moduleImpl._compile(source, modulePath);
+    implementation = moduleImpl.exports;
+  } catch (error1) {
+    e = error1;
+    callback(e);
+    return;
+  }
+  if ((typeof implementation !== 'function') && (typeof implementation.getComponent !== 'function')) {
+    callback(new Error('Provided source failed to create a runnable component'));
+    return;
+  }
 
-  loader.registerComponent packageId, name, implementation, callback
-  return
+  loader.registerComponent(packageId, name, implementation, callback);
+};
 
-exports.getSource = (loader, name, callback) ->
-  component = loader.components[name]
-  unless component
-    # Try an alias
-    for componentName of loader.components
-      if componentName.split('/')[1] is name
-        component = loader.components[componentName]
-        name = componentName
-        break
-    unless component
-      callback new Error "Component #{name} not installed"
-      return
+exports.getSource = function(loader, name, callback) {
+  let component = loader.components[name];
+  if (!component) {
+    // Try an alias
+    for (let componentName in loader.components) {
+      if (componentName.split('/')[1] === name) {
+        component = loader.components[componentName];
+        name = componentName;
+        break;
+      }
+    }
+    if (!component) {
+      callback(new Error(`Component ${name} not installed`));
+      return;
+    }
+  }
 
-  nameParts = name.split '/'
-  if nameParts.length is 1
-    nameParts[1] = nameParts[0]
-    nameParts[0] = ''
+  const nameParts = name.split('/');
+  if (nameParts.length === 1) {
+    nameParts[1] = nameParts[0];
+    nameParts[0] = '';
+  }
 
-  if loader.isGraph component
-    if typeof component is 'object'
-      if typeof component.toJSON is 'function'
-        callback null,
-          name: nameParts[1]
-          library: nameParts[0]
-          code: JSON.stringify component.toJSON()
+  if (loader.isGraph(component)) {
+    if (typeof component === 'object') {
+      if (typeof component.toJSON === 'function') {
+        callback(null, {
+          name: nameParts[1],
+          library: nameParts[0],
+          code: JSON.stringify(component.toJSON()),
           language: 'json'
-        return
-      callback new Error "Can't provide source for #{name}. Not a file"
-      return
-    fbpGraph.graph.loadFile component, (err, graph) ->
-      if err
-        callback err
-        return
-      return callback new Error 'Unable to load graph' unless graph
-      callback null,
-        name: nameParts[1]
-        library: nameParts[0]
-        code: JSON.stringify graph.toJSON()
+        }
+        );
+        return;
+      }
+      callback(new Error(`Can't provide source for ${name}. Not a file`));
+      return;
+    }
+    fbpGraph.graph.loadFile(component, function(err, graph) {
+      if (err) {
+        callback(err);
+        return;
+      }
+      if (!graph) { return callback(new Error('Unable to load graph')); }
+      return callback(null, {
+        name: nameParts[1],
+        library: nameParts[0],
+        code: JSON.stringify(graph.toJSON()),
         language: 'json'
-    return
+      }
+      );
+    });
+    return;
+  }
 
-  if typeof component isnt 'string'
-    callback new Error "Can't provide source for #{name}. Not a file"
-    return
+  if (typeof component !== 'string') {
+    callback(new Error(`Can't provide source for ${name}. Not a file`));
+    return;
+  }
 
-  fs.readFile component, 'utf-8', (err, code) ->
-    if err
-      callback err
-      return
-    callback null,
-      name: nameParts[1]
-      library: nameParts[0]
-      language: utils.guessLanguageFromFilename component
-      code: code
-    return
-  return
+  fs.readFile(component, 'utf-8', function(err, code) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    callback(null, {
+      name: nameParts[1],
+      library: nameParts[0],
+      language: utils.guessLanguageFromFilename(component),
+      code
+    }
+    );
+  });
+};
