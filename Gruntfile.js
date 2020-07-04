e9402061ed17bf23a4e3b24bf8fafbe8f6cb455a
Vladimir Sibirov <trustmaster@kodigy.com>
2020-07-04 12:21:37 +0200

    Update Gruntfile after decaf

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 module.exports = function() {
   // Project configuration
   this.initConfig({
