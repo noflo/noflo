4861629e140c82facf97a00fe19d892fabf9d383
decaffeinate <trustmaster@kodigy.com>
2020-07-11 12:54:24 +0200

    decaffeinate: Convert ComponentExample.coffee to JS

--- spec/ComponentExample.js
+++ spec/ComponentExample.js
@@ -1,93 +1,98 @@
-if typeof process isnt 'undefined' and process.execPath and process.execPath.match /node|iojs/
-  chai = require 'chai' unless chai
-  noflo = require '../src/lib/NoFlo'
-else
-  noflo = require 'noflo'
+let chai, noflo;
+if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
+  if (!chai) { chai = require('chai'); }
+  noflo = require('../src/lib/NoFlo');
+} else {
+  noflo = require('noflo');
+}
 
-describe 'MergeObjects component', ->
-  c = null
-  sin1 = null
-  sin2 = null
-  sin3 = null
-  sout1 = null
-  sout2 = null
-  obj1 =
-    name: 'Patrick'
+describe('MergeObjects component', function() {
+  let c = null;
+  let sin1 = null;
+  let sin2 = null;
+  let sin3 = null;
+  let sout1 = null;
+  let sout2 = null;
+  const obj1 = {
+    name: 'Patrick',
     age: 21
-  obj2 =
-    title: 'Attorney'
+  };
+  const obj2 = {
+    title: 'Attorney',
     age: 33
-  before (done) ->
-    if noflo.isBrowser()
-      @skip()
-      return
-    MergeObjects = require './components/MergeObjects.coffee'
-    c = MergeObjects.getComponent()
-    sin1 = new noflo.internalSocket.InternalSocket
-    sin2 = new noflo.internalSocket.InternalSocket
-    sin3 = new noflo.internalSocket.InternalSocket
-    sout1 = new noflo.internalSocket.InternalSocket
-    sout2 = new noflo.internalSocket.InternalSocket
-    c.inPorts.obj1.attach sin1
-    c.inPorts.obj2.attach sin2
-    c.inPorts.overwrite.attach sin3
-    c.outPorts.result.attach sout1
-    c.outPorts.error.attach sout2
-    done()
-    return
-  beforeEach (done) ->
-    sout1.removeAllListeners()
-    sout2.removeAllListeners()
-    done()
-    return
+  };
+  before(function(done) {
+    if (noflo.isBrowser()) {
+      this.skip();
+      return;
+    }
+    const MergeObjects = require('./components/MergeObjects.coffee');
+    c = MergeObjects.getComponent();
+    sin1 = new noflo.internalSocket.InternalSocket;
+    sin2 = new noflo.internalSocket.InternalSocket;
+    sin3 = new noflo.internalSocket.InternalSocket;
+    sout1 = new noflo.internalSocket.InternalSocket;
+    sout2 = new noflo.internalSocket.InternalSocket;
+    c.inPorts.obj1.attach(sin1);
+    c.inPorts.obj2.attach(sin2);
+    c.inPorts.overwrite.attach(sin3);
+    c.outPorts.result.attach(sout1);
+    c.outPorts.error.attach(sout2);
+    done();
+  });
+  beforeEach(function(done) {
+    sout1.removeAllListeners();
+    sout2.removeAllListeners();
+    done();
+  });
 
-  it 'should not trigger if input is not complete', (done) ->
-    sout1.once 'ip', (ip) ->
-      done new Error "Premature result"
-      return
-    sout2.once 'ip', (ip) ->
-      done new Error "Premature error"
-      return
+  it('should not trigger if input is not complete', function(done) {
+    sout1.once('ip', function(ip) {
+      done(new Error("Premature result"));
+    });
+    sout2.once('ip', function(ip) {
+      done(new Error("Premature error"));
+    });
 
-    sin1.post new noflo.IP 'data', obj1
-    sin2.post new noflo.IP 'data', obj2
+    sin1.post(new noflo.IP('data', obj1));
+    sin2.post(new noflo.IP('data', obj2));
 
-    setTimeout done, 10
-    return
+    setTimeout(done, 10);
+  });
 
-  it 'should merge objects when input is complete', (done) ->
-    sout1.once 'ip', (ip) ->
-      chai.expect(ip).to.be.an 'object'
-      chai.expect(ip.type).to.equal 'data'
-      chai.expect(ip.data).to.be.an 'object'
-      chai.expect(ip.data.name).to.equal obj1.name
-      chai.expect(ip.data.title).to.equal obj2.title
-      chai.expect(ip.data.age).to.equal obj1.age
-      done()
-      return
-    sout2.once 'ip', (ip) ->
-      done ip
-      return
+  it('should merge objects when input is complete', function(done) {
+    sout1.once('ip', function(ip) {
+      chai.expect(ip).to.be.an('object');
+      chai.expect(ip.type).to.equal('data');
+      chai.expect(ip.data).to.be.an('object');
+      chai.expect(ip.data.name).to.equal(obj1.name);
+      chai.expect(ip.data.title).to.equal(obj2.title);
+      chai.expect(ip.data.age).to.equal(obj1.age);
+      done();
+    });
+    sout2.once('ip', function(ip) {
+      done(ip);
+    });
 
-    sin3.post new noflo.IP 'data', false
-    return
+    sin3.post(new noflo.IP('data', false));
+  });
 
-  it 'should obey the overwrite control', (done) ->
-    sout1.once 'ip', (ip) ->
-      chai.expect(ip).to.be.an 'object'
-      chai.expect(ip.type).to.equal 'data'
-      chai.expect(ip.data).to.be.an 'object'
-      chai.expect(ip.data.name).to.equal obj1.name
-      chai.expect(ip.data.title).to.equal obj2.title
-      chai.expect(ip.data.age).to.equal obj2.age
-      done()
-      return
-    sout2.once 'ip', (ip) ->
-      done ip
-      return
+  it('should obey the overwrite control', function(done) {
+    sout1.once('ip', function(ip) {
+      chai.expect(ip).to.be.an('object');
+      chai.expect(ip.type).to.equal('data');
+      chai.expect(ip.data).to.be.an('object');
+      chai.expect(ip.data.name).to.equal(obj1.name);
+      chai.expect(ip.data.title).to.equal(obj2.title);
+      chai.expect(ip.data.age).to.equal(obj2.age);
+      done();
+    });
+    sout2.once('ip', function(ip) {
+      done(ip);
+    });
 
-    sin3.post new noflo.IP 'data', true
-    sin1.post new noflo.IP 'data', obj1
-    sin2.post new noflo.IP 'data', obj2
-    return
-  return
+    sin3.post(new noflo.IP('data', true));
+    sin1.post(new noflo.IP('data', obj1));
+    sin2.post(new noflo.IP('data', obj2));
+  });
+});
