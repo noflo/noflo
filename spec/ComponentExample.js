c20ab7937ca6572ad679e5cd7a670a7cbaec133f
decaffeinate <trustmaster@kodigy.com>
2020-07-11 12:54:26 +0200

    decaffeinate: Run post-processing cleanups on ComponentExample.coffee

--- spec/ComponentExample.js
+++ spec/ComponentExample.js
@@ -1,4 +1,15 @@
-let chai, noflo;
+/* eslint-disable
+    func-names,
+    global-require,
+    import/no-extraneous-dependencies,
+    import/no-unresolved,
+    no-undef,
+    no-unused-vars,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
+let chai; let
+  noflo;
 if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
   if (!chai) { chai = require('chai'); }
   noflo = require('../src/lib/NoFlo');
@@ -6,7 +17,7 @@ if ((typeof process !== 'undefined') && process.execPath && process.execPath.mat
   noflo = require('noflo');
 }
 
-describe('MergeObjects component', function() {
+describe('MergeObjects component', () => {
   let c = null;
   let sin1 = null;
   let sin2 = null;
@@ -15,24 +26,24 @@ describe('MergeObjects component', function() {
   let sout2 = null;
   const obj1 = {
     name: 'Patrick',
-    age: 21
+    age: 21,
   };
   const obj2 = {
     title: 'Attorney',
-    age: 33
+    age: 33,
   };
-  before(function(done) {
+  before(function (done) {
     if (noflo.isBrowser()) {
       this.skip();
       return;
     }
     const MergeObjects = require('./components/MergeObjects.coffee');
     c = MergeObjects.getComponent();
-    sin1 = new noflo.internalSocket.InternalSocket;
-    sin2 = new noflo.internalSocket.InternalSocket;
-    sin3 = new noflo.internalSocket.InternalSocket;
-    sout1 = new noflo.internalSocket.InternalSocket;
-    sout2 = new noflo.internalSocket.InternalSocket;
+    sin1 = new noflo.internalSocket.InternalSocket();
+    sin2 = new noflo.internalSocket.InternalSocket();
+    sin3 = new noflo.internalSocket.InternalSocket();
+    sout1 = new noflo.internalSocket.InternalSocket();
+    sout2 = new noflo.internalSocket.InternalSocket();
     c.inPorts.obj1.attach(sin1);
     c.inPorts.obj2.attach(sin2);
     c.inPorts.overwrite.attach(sin3);
@@ -40,18 +51,18 @@ describe('MergeObjects component', function() {
     c.outPorts.error.attach(sout2);
     done();
   });
-  beforeEach(function(done) {
+  beforeEach((done) => {
     sout1.removeAllListeners();
     sout2.removeAllListeners();
     done();
   });
 
-  it('should not trigger if input is not complete', function(done) {
-    sout1.once('ip', function(ip) {
-      done(new Error("Premature result"));
+  it('should not trigger if input is not complete', (done) => {
+    sout1.once('ip', (ip) => {
+      done(new Error('Premature result'));
     });
-    sout2.once('ip', function(ip) {
-      done(new Error("Premature error"));
+    sout2.once('ip', (ip) => {
+      done(new Error('Premature error'));
     });
 
     sin1.post(new noflo.IP('data', obj1));
@@ -60,8 +71,8 @@ describe('MergeObjects component', function() {
     setTimeout(done, 10);
   });
 
-  it('should merge objects when input is complete', function(done) {
-    sout1.once('ip', function(ip) {
+  it('should merge objects when input is complete', (done) => {
+    sout1.once('ip', (ip) => {
       chai.expect(ip).to.be.an('object');
       chai.expect(ip.type).to.equal('data');
       chai.expect(ip.data).to.be.an('object');
@@ -70,15 +81,15 @@ describe('MergeObjects component', function() {
       chai.expect(ip.data.age).to.equal(obj1.age);
       done();
     });
-    sout2.once('ip', function(ip) {
+    sout2.once('ip', (ip) => {
       done(ip);
     });
 
     sin3.post(new noflo.IP('data', false));
   });
 
-  it('should obey the overwrite control', function(done) {
-    sout1.once('ip', function(ip) {
+  it('should obey the overwrite control', (done) => {
+    sout1.once('ip', (ip) => {
       chai.expect(ip).to.be.an('object');
       chai.expect(ip.type).to.equal('data');
       chai.expect(ip.data).to.be.an('object');
@@ -87,7 +98,7 @@ describe('MergeObjects component', function() {
       chai.expect(ip.data.age).to.equal(obj2.age);
       done();
     });
-    sout2.once('ip', function(ip) {
+    sout2.once('ip', (ip) => {
       done(ip);
     });
 
