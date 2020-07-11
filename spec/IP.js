70186f1201fd2c61865989b7313663d7a8e186c1
decaffeinate <trustmaster@kodigy.com>
2020-07-11 12:55:10 +0200

    decaffeinate: Run post-processing cleanups on IP.coffee

--- spec/IP.js
+++ spec/IP.js
@@ -1,4 +1,13 @@
-let chai, noflo;
+/* eslint-disable
+    global-require,
+    import/no-extraneous-dependencies,
+    import/no-unresolved,
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
+let chai; let
+  noflo;
 if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
   if (!chai) { chai = require('chai'); }
   noflo = require('../src/lib/NoFlo');
@@ -6,35 +15,34 @@ if ((typeof process !== 'undefined') && process.execPath && process.execPath.mat
   noflo = require('noflo');
 }
 
-describe('IP object', function() {
-  it('should create IPs of different types', function() {
+describe('IP object', () => {
+  it('should create IPs of different types', () => {
     const open = new noflo.IP('openBracket');
-    const data = new noflo.IP('data', "Payload");
+    const data = new noflo.IP('data', 'Payload');
     const close = new noflo.IP('closeBracket');
     chai.expect(open.type).to.equal('openBracket');
     chai.expect(close.type).to.equal('closeBracket');
     chai.expect(data.type).to.equal('data');
   });
-  it('should be moved to an owner', function() {
-    const p = new noflo.IP('data', "Token");
+  it('should be moved to an owner', () => {
+    const p = new noflo.IP('data', 'Token');
     p.move('SomeProc');
     chai.expect(p.owner).to.equal('SomeProc');
   });
-  it('should support sync context scoping', function() {
-    const p = new noflo.IP('data', "Request-specific");
+  it('should support sync context scoping', () => {
+    const p = new noflo.IP('data', 'Request-specific');
     p.scope = 'request-12345';
     chai.expect(p.scope).to.equal('request-12345');
   });
-  it('should be able to clone itself', function() {
-    const d1 = new noflo.IP('data', "Trooper", {
+  it('should be able to clone itself', () => {
+    const d1 = new noflo.IP('data', 'Trooper', {
       groups: ['foo', 'bar'],
       owner: 'SomeProc',
       scope: 'request-12345',
       clonable: true,
       datatype: 'string',
-      schema: 'text/plain'
-    }
-    );
+      schema: 'text/plain',
+    });
     const d2 = d1.clone();
     chai.expect(d2).not.to.equal(d1);
     chai.expect(d2.type).to.equal(d1.type);
@@ -44,8 +52,8 @@ describe('IP object', function() {
     chai.expect(d2.owner).not.to.equal(d1.owner);
     chai.expect(d2.scope).to.equal(d1.scope);
   });
-  it('should dispose its contents when dropped', function() {
-    const p = new noflo.IP('data', "Garbage");
+  it('should dispose its contents when dropped', () => {
+    const p = new noflo.IP('data', 'Garbage');
     p.groups = ['foo', 'bar'];
     p.drop();
     chai.expect(Object.keys(p)).to.have.lengthOf(0);
