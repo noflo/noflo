9ee1acb5bf153675aaec2339e4b8aa8f33dd0625
decaffeinate <trustmaster@kodigy.com>
2020-07-12 12:56:46 +0200

    decaffeinate: Convert MergeObjects.coffee to JS

--- spec/components/MergeObjects.js
+++ spec/components/MergeObjects.js
@@ -1,44 +1,65 @@
-if typeof process isnt 'undefined' and process.execPath and process.execPath.match /node|iojs/
-  chai = require 'chai' unless chai
-  component = require '../../src/lib/Component.js'
-  socket = require '../../src/lib/InternalSocket.js'
-  IP = require '../../src/lib/IP.js'
-else
-  component = require 'noflo/src/lib/Component.js'
-  socket = require 'noflo/src/lib/InternalSocket.js'
-  IP = require 'noflo/src/lib/IP.js'
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+let component, IP, socket;
+if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
+  if (!chai) { var chai = require('chai'); }
+  component = require('../../src/lib/Component.js');
+  socket = require('../../src/lib/InternalSocket.js');
+  IP = require('../../src/lib/IP.js');
+} else {
+  component = require('noflo/src/lib/Component.js');
+  socket = require('noflo/src/lib/InternalSocket.js');
+  IP = require('noflo/src/lib/IP.js');
+}
 
-exports.getComponent = ->
-  c = new component.Component
-    desciption: 'Merges two objects into one (cloning)'
-    inPorts:
-      obj1:
-        datatype: 'object'
+exports.getComponent = function() {
+  const c = new component.Component({
+    desciption: 'Merges two objects into one (cloning)',
+    inPorts: {
+      obj1: {
+        datatype: 'object',
         desciption: 'First object'
-      obj2:
-        datatype: 'object'
+      },
+      obj2: {
+        datatype: 'object',
         desciption: 'Second object'
-      overwrite:
-        datatype: 'boolean'
-        desciption: 'Overwrite obj1 properties with obj2'
+      },
+      overwrite: {
+        datatype: 'boolean',
+        desciption: 'Overwrite obj1 properties with obj2',
         control: true
-    outPorts:
-      result:
+      }
+    },
+    outPorts: {
+      result: {
         datatype: 'object'
-      error:
+      },
+      error: {
         datatype: 'object'
+      }
+    }
+  });
 
-  c.process (input, output) ->
-    return unless input.has 'obj1', 'obj2', 'overwrite'
-    [obj1, obj2, overwrite] = input.getData 'obj1', 'obj2', 'overwrite'
-    try
-      src = JSON.parse JSON.stringify if overwrite then obj1 else obj2
-      dst = JSON.parse JSON.stringify if overwrite then obj2 else obj1
-    catch e
-      output.done e
-      return
-    for key, val of dst
-      src[key] = val
-    output.sendDone
-      result: src
-    return
+  return c.process(function(input, output) {
+    let dst, src;
+    if (!input.has('obj1', 'obj2', 'overwrite')) { return; }
+    const [obj1, obj2, overwrite] = Array.from(input.getData('obj1', 'obj2', 'overwrite'));
+    try {
+      src = JSON.parse(JSON.stringify(overwrite ? obj1 : obj2));
+      dst = JSON.parse(JSON.stringify(overwrite ? obj2 : obj1));
+    } catch (e) {
+      output.done(e);
+      return;
+    }
+    for (let key in dst) {
+      const val = dst[key];
+      src[key] = val;
+    }
+    output.sendDone({
+      result: src});
+  });
+};
