f1ee24a1ab41b47dd6fc82266f004ad45ccae81d
decaffeinate <trustmaster@kodigy.com>
2020-07-12 12:46:01 +0200

    decaffeinate: Convert Output.coffee to JS

--- spec/fixtures/componentloader/components/Output.js
+++ spec/fixtures/componentloader/components/Output.js
@@ -1,16 +1,17 @@
-noflo = require '../../../../src/lib/NoFlo'
+const noflo = require('../../../../src/lib/NoFlo');
 
-exports.getComponent = ->
-  c = new noflo.Component
-  c.description = "Output stuff"
-  c.inPorts.add 'in',
-    datatype: 'string'
-  c.inPorts.add 'out',
-    datatype: 'string'
-  c.process = (input, output) ->
-    data = input.getData 'in'
-    console.log data
-    output.sendDone
-      out: data
-    return
-  return c
+exports.getComponent = function() {
+  const c = new noflo.Component;
+  c.description = "Output stuff";
+  c.inPorts.add('in',
+    {datatype: 'string'});
+  c.inPorts.add('out',
+    {datatype: 'string'});
+  c.process = function(input, output) {
+    const data = input.getData('in');
+    console.log(data);
+    output.sendDone({
+      out: data});
+  };
+  return c;
+};
