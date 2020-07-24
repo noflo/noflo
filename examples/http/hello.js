265a55df1f4496b3f691440d83ef20ba5c37b6a9
Vladimir Sibirov <trustmaster@kodigy.com>
2020-07-24 21:50:39 +0200

    Cleanup http example

--- examples/http/hello.js
+++ examples/http/hello.js
@@ -1,5 +1,3 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
 // Flow-based example of serving web pages
 
 const noflo = require("noflo");
