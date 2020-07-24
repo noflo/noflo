8c550dd1375292b736b89cc2e0dcc52eb651f489
decaffeinate <trustmaster@kodigy.com>
2020-07-24 21:47:43 +0200

    decaffeinate: Run post-processing cleanups on hello.coffee

--- examples/http/hello.js
+++ examples/http/hello.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 // Flow-based example of serving web pages
 
 const noflo = require("noflo");
