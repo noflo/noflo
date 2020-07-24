847de043bebfa316d8505ce55981fac792ccabb7
decaffeinate <trustmaster@kodigy.com>
2020-07-24 21:47:25 +0200

    decaffeinate: Convert HelloController.coffee to JS

--- examples/http/HelloController.js
+++ examples/http/HelloController.js
@@ -1,21 +1,25 @@
-noflo = require "noflo"
+const noflo = require("noflo");
 
-exports.getComponent = ->
-  c = new noflo.Component
-  c.description = "Simple controller that says hello, user"
-  c.inPorts.add 'in',
-    datatype: 'object'
-  c.outPorts.add 'out',
-    datatype: 'object'
-  c.outPorts.add 'data',
-    datatype: 'object'
-  c.process (input, output) ->
-    return unless input.hasData 'in'
-    request = input.getData 'in'
-    output.sendDone
-      out: request
-      data:
-        locals:
-          string: "Hello, #{request.req.remoteUser}"
-    return
-  return c
+exports.getComponent = function() {
+  const c = new noflo.Component;
+  c.description = "Simple controller that says hello, user";
+  c.inPorts.add('in',
+    {datatype: 'object'});
+  c.outPorts.add('out',
+    {datatype: 'object'});
+  c.outPorts.add('data',
+    {datatype: 'object'});
+  c.process(function(input, output) {
+    if (!input.hasData('in')) { return; }
+    const request = input.getData('in');
+    output.sendDone({
+      out: request,
+      data: {
+        locals: {
+          string: `Hello, ${request.req.remoteUser}`
+        }
+      }
+    });
+  });
+  return c;
+};
