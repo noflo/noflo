e91f137d15954783c7d939d0387a1abb8771af9f
decaffeinate <trustmaster@kodigy.com>
2020-07-24 21:52:52 +0200

    decaffeinate: Convert count.coffee to JS

--- examples/linecount/count.js
+++ examples/linecount/count.js
@@ -1,30 +1,32 @@
-# Flow-based example of counting lines of a file, roughly equivalent to
-# "wc -l <filename>"
+// Flow-based example of counting lines of a file, roughly equivalent to
+// "wc -l <filename>"
 
-noflo = require "noflo"
+const noflo = require("noflo");
 
-unless process.argv[2]
-    console.error "You must provide a filename"
-    process.exit 1
+if (!process.argv[2]) {
+    console.error("You must provide a filename");
+    process.exit(1);
+  }
 
-fileName = process.argv[2]
+const fileName = process.argv[2];
 
-graph = noflo.graph.createGraph "linecount"
-graph.addNode "Read File", "ReadFile"
-graph.addNode "Split by Lines", "SplitStr"
-graph.addNode "Count Lines", "Counter"
-graph.addNode "Display", "Output"
+const graph = noflo.graph.createGraph("linecount");
+graph.addNode("Read File", "ReadFile");
+graph.addNode("Split by Lines", "SplitStr");
+graph.addNode("Count Lines", "Counter");
+graph.addNode("Display", "Output");
 
-graph.addEdge "Read File", "out", "Split by Lines", "in"
-#graph.addEdge "Read File", "error", "Display", "in"
-graph.addEdge "Split by Lines", "out", "Count Lines", "in"
-graph.addEdge "Count Lines", "count", "Display", "in"
+graph.addEdge("Read File", "out", "Split by Lines", "in");
+//graph.addEdge "Read File", "error", "Display", "in"
+graph.addEdge("Split by Lines", "out", "Count Lines", "in");
+graph.addEdge("Count Lines", "count", "Display", "in");
 
-# Kick the process off by sending filename to fileReader
-graph.addInitial fileName, "Read File", "in"
+// Kick the process off by sending filename to fileReader
+graph.addInitial(fileName, "Read File", "in");
 
-noflo.createNetwork graph, (err) ->
-  if err
-    console.error err
-    process.exit 1
-  return
+noflo.createNetwork(graph, function(err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
