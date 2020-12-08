5ad258f8fd04fc7ae62fe593cc1a78894a4b3d56
Henri Bergius <henri.bergius@iki.fi>
2020-12-08 10:04:01 +0100

    Switch to JS modules

--- src/lib/Platform.js
+++ src/lib/Platform.js
@@ -9,18 +9,22 @@
 */
 
 // Platform detection method
-exports.isBrowser = function isBrowser() {
+export function isBrowser() {
   if ((typeof process !== 'undefined') && process.execPath && process.execPath.match(/node|iojs/)) {
     return false;
   }
   return true;
-};
+}
 
 // Mechanism for showing API deprecation warnings. By default logs the warnings
 // but can also be configured to throw instead with the `NOFLO_FATAL_DEPRECATED`
 // env var.
-exports.deprecated = function deprecated(message) {
-  if (exports.isBrowser()) {
+/**
+ * @param {string} message
+ * @returns {void}
+ */
+export function deprecated(message) {
+  if (isBrowser()) {
     console.warn(message);
     return;
   }
